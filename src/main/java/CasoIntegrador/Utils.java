e0ab46c0bf2c3353c5d332f96a76ff4c6ce7b0f3
PaxuitoGIT <bxu@myuax.com>
2024-05-08 12:02:01 +0200

    Método leerCadena Utils

--- src/main/java/CasoIntegrador/Utils.java
+++ src/main/java/CasoIntegrador/Utils.java
@@ -1,6 +1,15 @@
 package CasoIntegrador;
 
+import java.util.Scanner;
+
 public class Utils {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String leerCadena(String mensaje) {
+        System.out.print(mensaje);
+        return scanner.nextLine();
+    }
+
     public static boolean isValidAlias(String alias) {
         return alias.matches("[a-zA-Z0-9]+");
     }
