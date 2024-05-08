f911dc5566adf6582e81b31a1e16729b39a477ab
PaxuitoGIT <bxu@myuax.com>
2024-05-08 11:09:30 +0200

    Constructor UserAccount

--- src/main/java/CasoIntegrador/UserAccount.java
+++ src/main/java/CasoIntegrador/UserAccount.java
@@ -9,4 +9,18 @@ public class UserAccount {
     private ArrayList<UserAccount> seguidores;
     private ArrayList<UserAccount> seguidos;
 
+    public UserAccount(String alias, String email) {
+        if (!Utils.isValidAlias(alias)) {
+            throw new IllegalArgumentException("El alias no es válido.");
+        }
+        if (!Utils.isValidEmail(email)) {
+            throw new IllegalArgumentException("El email no es válido.");
+        }
+
+        this.alias = alias;
+        this.email = email;
+        this.tweets = new ArrayList<>();
+        this.seguidores = new ArrayList<>();
+        this.seguidos = new ArrayList<>();
+    }
 }
