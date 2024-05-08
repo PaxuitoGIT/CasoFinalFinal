6625da18d1a16c69e67857613f22eadf8b17cacd
PaxuitoGIT <bxu@myuax.com>
2024-05-08 11:33:59 +0200

    Atributo, constructor y getter y setter Retweet

---
+++ src/main/java/CasoIntegrador/Retweet.java
@@ -0,0 +1,18 @@
+package CasoIntegrador;
+
+public class Retweet extends Tweet{
+    private Tweet tweetOriginal;
+
+    public Retweet(String mensaje, UserAccount autor, Tweet tweetOriginal) {
+        super(mensaje, autor);
+        this.tweetOriginal = tweetOriginal;
+    }
+
+    public Tweet getTweetOriginal() {
+        return tweetOriginal;
+    }
+
+    public void setTweetOriginal(Tweet tweetOriginal) {
+        this.tweetOriginal = tweetOriginal;
+    }
+}
