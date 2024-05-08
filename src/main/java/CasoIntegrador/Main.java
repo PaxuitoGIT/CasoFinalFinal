9a580a13c7ad658ce763adc23a0f36a51dccd818
PaxuitoGIT <bxu@myuax.com>
2024-05-08 11:03:57 +0200

    atributos UserAccount

--- src/main/java/CasoIntegrador/Main.java
+++ src/main/java/CasoIntegrador/Main.java
@@ -1,10 +1,7 @@
 package CasoIntegrador;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public class Main
 {
     public static void main( String[] args )
     {
---
+++ src/main/java/CasoIntegrador/UserAccount.java
@@ -0,0 +1,12 @@
+package CasoIntegrador;
+
+import java.util.ArrayList;
+
+public class UserAccount {
+    private String alias;
+    private String email;
+    private ArrayList<Tweet> tweets;
+    private ArrayList<UserAccount> seguidores;
+    private ArrayList<UserAccount> seguidos;
+
+}
